44083983f22cf663acdca61bcaf86509d767860d
Osarobari Hope <mariushope007@gmail.com>
2022-10-24 12:56:38 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,16 @@
 
 void print_rev(char *s)
 {
-	int i, n; 
-	
+	int i, n;
+
 	n = 0;
-			while (s[n] != '\0')
-						n++;
-			for (i = n - 1; i >= 0; i--)
-					{
-								_putchar(s[i]);
-									}
-				_putchar('\n');
+
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
 }
