19f51e8cbd96d5bd92af800e33adcf7f244bec1e
Osarobari Hope <mariushope007@gmail.com>
2022-10-24 21:12:29 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,15 @@
 yvoid rev_string(char *s)
 {
 	int a = 0, b, c;
-	
-	char d; 
+
+	char d;
 		while (s[a] != '\0')
 			{
 			a++;
 			}
-	c = a -1;
+	c = a - 1;
 
-	for ( b = 0; c >= 0 && b < c; c--, b++)
+	for (b = 0; c >= 0 && b < c; c--, b++)
 	{
 		d = s[b];
 		s[b] = s[c];
