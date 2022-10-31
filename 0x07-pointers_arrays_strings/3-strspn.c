22048cc76dd6bb679e87c079b01abb95c5fdd4fb
Osarobari Hope <mariushope007@gmail.com>
2022-10-31 14:56:45 -0700

    merit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int z = 0, x, y;
-	
+
 	for (x = 0; s[x] != '\0'; x++)
 	{
 		if (s[x] != 32)
 		{
-			for (y = 0; accept[y] != '\0'; y++)
+		for (y = 0; accept[y] != '\0'; y++)
 		}
-		
 		if (s[x] == accept[y])
-									z++;																									}
-																	}
-										else
-														return (z);
-											}
-					return (z);
+
+			z++;
+	}
+	else
+		return (z);
+}
+return (z);
 
 }
