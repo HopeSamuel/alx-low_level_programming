16060a52e81116564c1506e553112aae02bf4b4d
Osarobari Hope <mariushope007@gmail.com>
2022-11-08 16:58:34 -0800

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,8 +33,10 @@ char *str_concat(char *s1, char *s2)
 	}
 	len = len1 + len2;
 	result = (char *)malloc(sizeof(char) * (len + 1));
+
 	if (result == NULL)
 		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i]
 	for (j = 0; j < len2; j++, i++)
