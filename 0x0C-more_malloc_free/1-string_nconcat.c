2fcfd5176580a84206080c8deaad83085225f918
Osarobari Hope <mariushope007@gmail.com>
2022-11-10 16:26:18 -0800

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,26 +10,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int x, y, z;
-	
+
 	char *s;
-	
+
 	if (s1 == NULL)
 	{
 		x = 0;
 	}
 	else
 	{
-		for (x = 0; s1[x]; ++x) 
+		for (x = 0; s1[x]; ++x)
 		;
 	}
-	
+
 	if (s2 == NULL)
 	{
 		y = 0;
 	}
-	
+
 	else
-	
+
 	{
 		for (y = 0; s2[y]; ++y)
 		;
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (z = 0; z < x; z++)
 		s[z] = s1[z];
-	
+
 	for (z = 0; z < y; z++)
 		s[z + x] = s2[z];
 	s[x + y] = '\0';
@@ -61,15 +61,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	unsigned int x, y, z;
 	char *s;
-	
+
 	if (s1 == NULL)
 	{
 		x = 0;
 	}
 	else
 	{
-		for (x = 0; s1[x]; ++x;
-		;
+		for (x = 0; s1[x]; ++x);
 	}
 	if (s2 == NULL)
 	{
@@ -77,8 +76,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-	for (y = 0; s2[y]; ++y)					
-	;
+	for (y = 0; s2[y]; ++y);
 	}
 	if (y > n)
 	y = n;
