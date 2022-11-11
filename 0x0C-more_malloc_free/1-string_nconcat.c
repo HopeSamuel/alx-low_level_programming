fcb76e4332a92367baf4feca7c51cc2ebc658df4
Osarobari Hope <mariushope007@gmail.com>
2022-11-10 16:52:18 -0800

    task2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -68,25 +68,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for (x = 0; s1[x]; ++x);
+		for (x = 0; s1[x]; ++x)
+		;
 	}
 	if (s2 == NULL)
 	{
-	y = 0;
+		y = 0;
 	}
 	else
 	{
-	for (y = 0; s2[y]; ++y);
+	for (y = 0; s2[y]; ++y)
+	;
 	}
 	if (y > n)
-	y = n;
+		y = n;
 	s = malloc(sizeof(char) * (x + y + 1));
 	if (s == NULL)
-	return (NULL);
+		return (NULL);
 	for (z = 0; z < x; z++)
-	s[z] = s1[z];
+		s[z] = s1[z];
 	for (z = 0; z < y; z++)
-	s[z + x] = s2[z];
+		s[z + x] = s2[z];
 	s[x + y] = '\0';
 	return (s);
 }
