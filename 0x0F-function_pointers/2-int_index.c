15811c63b9a0e66aa2939adc46172f415212ef62
Osarobari Hope <mariushope007@gmail.com>
2022-11-16 14:49:51 -0800

    trt

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	
+
 	if (size > 0)
 	{
 		if (array != NULL && cmp != NULL)
@@ -19,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 			{
 				if (cmp(array[i]))
 					return (i);
-				i++;
+			/\s\+$//ei++;
 			}
 		}
 	}
