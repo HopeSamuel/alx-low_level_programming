5510c73db88c376c1e677bee56a425df43b4e44b
Osarobari Hope <mariushope007@gmail.com>
2022-11-17 19:20:35 -0800

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
 	unsigned int i;
-	
+
 	va_list ap;
+
 	va_start(ap, n);
-	
+
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
+
 	va_end(ap);
+
 	return (sum);
