79dacb7bec5c0d043cb6b1581fcb65bdcb4bfbb6
Osarobari Hope <mariushope007@gmail.com>
2022-11-17 20:26:10 -0800

    work

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,20 +10,20 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	
+
 	va_list ap;
-	
+
 	va_start(ap, n);
-	
+
 	if (separator == NULL)
 		separator = "";
-	
+
 	for (i = 0; i < n; i++)
-		
+	{
 		printf("%d", va_arg(ap, int));
 	if (i < n - 1)
 		printf("%s", separator);
-}
+	}
 printf("\n");
 va_end(ap);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	unsigned int i;
 	va_list ap;
-	
+
 	va_start(ap, n);
 	if (separator == NULL)
 		separator = "";
