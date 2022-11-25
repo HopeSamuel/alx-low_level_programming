f2c4e906ef8406dbe4ab2a7da6bfb170f1d95f2a
Osarobari Hope <mariushope007@gmail.com>
2022-11-25 08:22:20 -0800

    task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,27 +22,26 @@ voiid print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(a_list, int));
-				
-					flag = 0;
-					break;
+				flag = 0;
+				break;
 			case 'i':
-					printf("%i", va_arg(a_list, int));
-					flag = 0;
-					break;	
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;	
 			case 'f':
-					printf("%f", va_arg(a_list, double));
-					flag = 0;
-					break;
-			case 's';
-					str = va_arg(a_list, char*);
-					if (str == NULL)
-						str = "(nil)";
-					printf("%s", str);
-					flag = 0;
-					break;
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
 			default:
-					flag = 1;
-					break;
+				flag = 1;
+				break;
 		}
 		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
