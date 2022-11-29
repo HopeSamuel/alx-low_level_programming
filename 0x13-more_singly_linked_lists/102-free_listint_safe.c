51090047c989d70afed52eb9ea998ddddbc7ae6b
Osarobari Hope <mariushope007@gmail.com>
2022-11-29 13:11:46 -0800

    5

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,18 +19,22 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 	if (newlist == NULL)
 	{
 		free(list);
+
 		exit(98);
 	}
+
 	for (i = 0; i < size - 1; i++)
+
 		newlist[i] = list[i];
+
 	newlist[i] = new;
 	free(list);
+
 	return (newlist);
 }
 
-
 /**
- * free_listint_safe - frees a listint_t linked list
+ * free_listint_safe - frees a listint_t linked list.
  * @head: double pointer to the start of the list
  * Return: the number of nodes in the list
  */
@@ -43,6 +47,7 @@ size_t free_listint_safe(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return (num);
 	while (*head != NULL)
+
 	{
 		for (i = 0; i < num; i++)
 		{
