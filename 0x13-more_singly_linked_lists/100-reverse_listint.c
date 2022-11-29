85b2c3ceb62853ba567704da04679c34e7fc810f
Osarobari Hope <mariushope007@gmail.com>
2022-11-28 19:05:23 -0800

    tp

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,10 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- *  * reverse_listint - the function that reverses a listint_t
- *   * @head: double pointer
- *    *
- *     * Return: a pointer to the first node of the reversed list
- *      */
+ * reverse_listint - the function that reverses a listint_t
+ * @head: double pointer
+ * Return: a pointer to the first node of the reversed list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *previous, *next;
@@ -24,5 +23,5 @@ listint_t *reverse_listint(listint_t **head)
 	}
 	*head = previous;
 	return (*head);
-}}
+}
 
