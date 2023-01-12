3a0ecdc8ddc039be00cd94a66afe9191204eb016
Osarobari Hope <mariushope007@gmail.com>
2023-01-12 09:32:41 -0800

    oh God

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+nclude "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to the beginning of the linked list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
